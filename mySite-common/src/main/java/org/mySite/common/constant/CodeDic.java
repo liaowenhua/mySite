7e8cdd4c064dcae81648118f116b65ab6c2c430c
wb-liaowenhua <wb-liaowenhua@le.com>
2018-09-17 08:50:57 +0800

    风险比例修正

--- mySite-common/src/main/java/org/mySite/common/constant/CodeDic.java
+++ mySite-common/src/main/java/org/mySite/common/constant/CodeDic.java
@@ -16,15 +16,15 @@ public class CodeDic {
 //    private static int n7 = 19;
 //    public static int max = 20;
 
-    public static int min = 6;
-    private static int n2 = 7;
-    private static int n3 = 8;
-    private static int n4 = 9;
-    private static int n5 = 10;
-    private static int n6 = 11;
-    private static int n7 = 12;
-    public static int max = 13;
-    public static double maxLost = -3048.00;
+    public static int min = 10;
+    private static int n2 = 11;
+    private static int n3 = 12;
+    private static int n4 = 13;
+    private static int n5 = 14;
+    private static int n6 = 15;
+    private static int n7 = 16;
+    public static int max = 17;
+    public static double maxLost = -1448.00;
 
     //对应规则
     public static final String code_map_0 = "0369";
@@ -46,28 +46,28 @@ public class CodeDic {
     //key 遗漏期数 value 累计收益
     private static Map<Integer, Double> profileMap = new HashMap<Integer, Double>();
     static {
-        profileMap.put(min, 23.04);
-        profileMap.put(n2, 30.08);
-        profileMap.put(n3, 67.2);
-        profileMap.put(n4, 171.52);
-        profileMap.put(n5, 21.12);
-        profileMap.put(n6, 38.08);
-        profileMap.put(n7, 55.36);
-        profileMap.put(max, 94.72);
+        profileMap.put(min, 11.52);
+        profileMap.put(n2, 15.04);
+        profileMap.put(n3, 33.6);
+        profileMap.put(n4, 85.76);
+        profileMap.put(n5, 4.8);
+        profileMap.put(n6, 11.52);
+        profileMap.put(n7, 10.88);
+        profileMap.put(max, 16.0);
         profileMap.put(max + 1, maxLost);
     }
 
     //key 遗漏期数  value 倍数
     public static Map<Integer, Integer> priceMap = new HashMap<Integer, Integer>();
     static {
-        priceMap.put(min, 2);
-        priceMap.put(n2, 4);
-        priceMap.put(n3, 10);
-        priceMap.put(n4, 26);
-        priceMap.put(n5, 31);
-        priceMap.put(n6, 54);
-        priceMap.put(n7, 93);
-        priceMap.put(max, 161);
+        priceMap.put(min, 1);
+        priceMap.put(n2, 2);
+        priceMap.put(n3, 5);
+        priceMap.put(n4, 13);
+        priceMap.put(n5, 15);
+        priceMap.put(n6, 26);
+        priceMap.put(n7, 44);
+        priceMap.put(max, 75);
     }
 
     public static double getProfile(int absent) {
